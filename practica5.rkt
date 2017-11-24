eebaba6ab7056c225828e48a073dbdd1a0024b59
Fernando Sánchez <fernandosd24@hotmail.com>
2017-11-24 11:56:54 +0100

    Refactorizado asignatura

--- practica5.rkt
+++ practica5.rkt
@@ -52,15 +52,37 @@
 
 ;;
 ;; Función de creación
+
+;; Crea una asignatura sin nombre ni estudiantes
 (define (crear-asignatura-vacia)
-  ;; Una asignatura es una lista de estudiantes
-  '()
+  ;; Una asignatura es una lista de estudiantes y su nombre
+  (list
+   (list 'nombre "")
+   (list 'estudiantes '())
+   )
+  )
+
+
+(define (crear-asignatura nombre)
+  (list
+   (list 'nombre nombre)
+   (list 'estudiantes '())
+   )
   )
 
 ;;
 ;; Funciones de consulta
+;;
+(define (ver-estudiantes asignatura)
+  (cadr (assoc 'estudiantes asignatura))
+  )
+
+(define (ver-nombre-asig asignatura)
+  (cadr (assoc 'nombre asignatura))
+  )
+
 (define (asignatura-vacia? asignatura)
-  (null? asignatura)
+  (null? (ver-estudiantes asignatura))
   )
 
 ;;
@@ -83,36 +105,54 @@
   )
 
 ;;
+;; Funciones de modificación
+;;
+
+;; Cambia el nombre de la asignatura por el recibido
+(define (cambiar-nombre-asig! asignatura nuevo)
+  (set-car! (cadr (assoc 'nombre asignatura)) nuevo)
+  )
+
+;; Cambia la lista de estudiantes que pertenecen a la asignatura
+(define (cambiar-estudiantes! asignatura lista-estudiantes)
+  (set-car! (cadr (assoc 'estudiantes asignatura)) lista-estudiantes)
+  )
+
+;;
 ;; Nombre: insertar-estudiante!
 ;; Resultado: inserta por orden alfabético el estudiante en la lista
 ;; Descripción: si el estudiante ya existe no cambia nada, si no lo inserta en su sitio
 ;;
 (define (insertar-estudiante! asignatura estudiante)
-  ;; Comprueba si existe el estudiante
-      (cond
-        ;; Comprueba si no hay estudiantes en la lista para insertar directamente
-        ((null? asignatura)
-         (cons estudiante asignatura)
-         )
-        ;; Comprueba si sus apellidos son menores
-        ((string-ci<? (ver-apellidos estudiante) (ver-apellidos (car asignatura)))
-         (cons estudiante asignatura)
-         )
-        ;; En caso de tener los mismos apellidos comprueba si es menor por nombre
-        ((and
-          (string-ci=? (ver-apellidos estudiante) (ver-apellidos (car asignatura)))
-          (string-ci<=? (ver-nombre estudiante) (ver-nombre (car asignatura)))
-          )
-         (if (string-ci=? (ver-nombre estudiante) (ver-nombre (car asignatura)))
-             (begin (display "hola")asignatura)
-             (cons estudiante asignatura)
-             )
-         )
-        ;; En caso de no poder insertarlo, pasa a comparar con el siguiente
-        (else
-         (cons (car asignatura) (insertar-estudiante! (cdr asignatura) estudiante))
-         )
+  (define (auxiliar lista-estudiantes estudiante)
+    ;; Comprueba si existe el estudiante
+    (cond
+      ;; Comprueba si no hay estudiantes en la lista para insertar directamente
+      ((null? lista-estudiantes)
+       (cons estudiante lista-estudiantes)
+       )
+      ;; Comprueba si sus apellidos son menores
+      ((string-ci<? (ver-apellidos estudiante) (ver-apellidos (car lista-estudiantes)))
+       (cons estudiante lista-estudiantes)
+       )
+      ;; En caso de tener los mismos apellidos comprueba si es menor por nombre
+      ((and
+        (string-ci=? (ver-apellidos estudiante) (ver-apellidos (car lista-estudiantes)))
+        (string-ci<=? (ver-nombre estudiante) (ver-nombre (car lista-estudiantes)))
         )
+       (if (string-ci=? (ver-nombre estudiante) (ver-nombre (car lista-estudiantes)))
+           (begin (display "hola") lista-estudiantes)
+           (cons estudiante lista-estudiantes)
+           )
+       )
+      ;; En caso de no poder insertarlo, pasa a comparar con el siguiente
+      (else
+       (cons (car asignatura) (auxiliar (cdr asignatura) estudiante))
+       )
+      )
+    )
+  (set-cdr! (assoc 'estudiantes asignatura) (list (auxiliar (ver-estudiantes asignatura) estudiante)))
+  (display asignatura)
   )
 
 (define (borrar-estudiante! asignatura estudiante)
@@ -137,7 +177,7 @@
 ;;
 (define (modificar-estudiante! asignatura objetivo nuevo)
   (cond
-    ((null? asignatura) '())
+    ((asignatura-vacia? asignatura) '())
     (else
      (insertar-estudiante! (borrar-estudiante! asignatura objetivo) nuevo)
      )
@@ -156,24 +196,35 @@
 
 (define (consultar-estudiantes asignatura)
   (display "Estudiantes de la asignatura:")(newline)
-  (map (lambda (estudiante) (newline)(consultar-estudiante estudiante)) asignatura)
+  (map (lambda (estudiante) (newline)(consultar-estudiante estudiante)) (ver-estudiantes asignatura))
   (newline)
   )
 
 (define (consultar-estudiantes-tipo asignatura tipo)
-  (cond
-    ((null? asignatura));; Termina la ejecución
-    ((string-ci=? tipo (ver-tipo (car asignatura)))
-     (consultar-estudiante (car asignatura))
-     )
+  (define (auxiliar lista-estudiantes)
+    (cond
+      ((asignatura-vacia? asignatura));; Termina la ejecución
+      ((string-ci=? tipo (ver-tipo (car lista-estudiantes)))
+       (consultar-estudiante (car lista-estudiantes))
+       )
+      )
     )
+  (auxiliar (ver-estudiantes asignatura))
   )
 
-(define asig (insertar-estudiante! '() (crear-estudiante "ana" "salas" "propio")))
-(define asig1 (insertar-estudiante! asig (crear-estudiante "ana" "perez" "erasmus")))
-(define asig2 (insertar-estudiante! asig1 (crear-estudiante "paco" "perez" "propio")))
-(define asig3 (insertar-estudiante! asig2 (crear-estudiante "paco" "zamora" "propio")))
-(modificar-estudiante! asig3 (crear-estudiante "PaCo" "pERez" "propio") (crear-estudiante "pepe" "xiez" "erasmus"))
-(consultar-estudiantes asig3)
+
+(define asignatura (crear-asignatura-vacia))
+(insertar-estudiante! asignatura (crear-estudiante "ana" "salas" "propio"))
+(display "1\n")
+(insertar-estudiante! asignatura (crear-estudiante "ana" "perez" "erasmus"))
+(display "2\n")
+(insertar-estudiante! asignatura (crear-estudiante "paco" "perez" "propio"))
+(display "3\n")
+(insertar-estudiante! asignatura (crear-estudiante "paco" "zamora" "propio"))
+(display "4\n")
+;(display (borrar-estudiante! asig3 (crear-estudiante "PaCo" "pERez" "propio")))
+(display "\n##------------\n")
+;(modificar-estudiante! asig3 (crear-estudiante "PaCo" "pERez" "propio") (crear-estudiante "pepe" "xiez" "erasmus"))
+(consultar-estudiantes asignatura)
 (display "-----------\n")
-(consultar-estudiantes-tipo asig3 "erasmus")
+(consultar-estudiantes-tipo asignatura "erasmus")
